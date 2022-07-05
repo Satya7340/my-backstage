1c20ad542779acadc01321364e9bf280ba074a92
Charles Moulliard <cmoulliard@redhat.com>
2022-07-05 07:33:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Backstage](https://backstage.io)
+
+This is your newly scaffolded Backstage Index, Good Luck!
+
+To start the app, run:
+
+```sh
+yarn install
+yarn dev
+```
---
+++ packages/backend/src/plugins/kubernetes.ts
@@ -0,0 +1,14 @@
+// ADDED for k8s plugin
+import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+export default async function createPlugin(
+    env: PluginEnvironment,
+): Promise<Router> {
+    const { router } = await KubernetesBuilder.createBuilder({
+        logger: env.logger,
+        config: env.config,
+    }).build();
+    return router;
+}
